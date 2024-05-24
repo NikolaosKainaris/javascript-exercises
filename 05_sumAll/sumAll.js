0641a0d9167cfe5ed5dc7e9c7ecf07f6d5e74c44
Nikolaos Kainaris <niko.kainaris@gmx.de>
2024-05-24 19:33:04 +0200

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR"
+    }
+    let start = Math.min(x, y)
+    let end = Math.max(x,y)
+
+    if (start < 0){
+        return "ERROR"
+    }
+
+    let array = [];
+    for (let i = start; i<=end; i++){
+        array.push(i)
+    }
+    return array.reduce((i, j) => i+j, 0)
 
 };
 
