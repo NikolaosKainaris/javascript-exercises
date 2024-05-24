1e56ee444a8e978b602855f8c304546a2039d37c
Nikolaos Kainaris <niko.kainaris@gmx.de>
2024-05-24 16:48:35 +0200

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberRepetitions) {
+    if (numberRepetitions < 0){
+        return "ERROR"
+    }
+    result = "";
+    for (let i=0; i<numberRepetitions; i++){
+        result += string;
+    }
+    return result
 };
 
 // Do not edit below this line
