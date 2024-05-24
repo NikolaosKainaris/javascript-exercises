8da53a19a6ca36e31a2590d1e629c092a2ce3239
Nikolaos Kainaris <niko.kainaris@gmx.de>
2024-05-24 19:33:16 +0200

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundToOneDecimal = function(x) {
+  return Math.round(x*10)/10
+}
+
+const convertToCelsius = function(temperatureCelcius) {
+  return roundToOneDecimal((temperatureCelcius - 32) * 5/9)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureFahrenheit) {
+  return roundToOneDecimal((temperatureFahrenheit * 9/5) + 32)
 };
 
 // Do not edit below this line
