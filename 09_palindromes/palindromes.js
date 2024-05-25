f97a36c401c97630ff0a67bcdf5c3515c21d362a
Nikolaos Kainaris <niko.kainaris@gmx.de>
2024-05-25 14:01:16 +0200

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const chars = word.split("").filter(char => /[a-zA-Z0-9]/.test(char))
+    for (let i = 0; i < chars.length / 2; i++) {
+        let first = chars[i].toLowerCase()
+        let second = chars[chars.length -1 - i].toLowerCase()
+       if (first !== second) return false
+    }
+    return true
 };
 
 // Do not edit below this line
