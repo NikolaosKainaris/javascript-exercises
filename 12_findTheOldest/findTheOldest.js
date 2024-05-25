62f54280617b7cdf839c30806e7f56e4c9fcfa85
Nikolaos Kainaris <niko.kainaris@gmx.de>
2024-05-25 14:01:35 +0200

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const age = function(person) {
+    if (person.yearOfDeath === undefined) person.yearOfDeath = new Date().getFullYear()
+    return person.yearOfDeath - person.yearOfBirth
+}
+const findTheOldest = function(people) {
+    return people.sort((p1, p2) => age(p1) < age(p2) ? 1 : -1)[0]
 };
 
 // Do not edit below this line
